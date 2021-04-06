4c0cf393b6c2ecc8d6171ce0c73c8067c6e6ac26
Arijit Aich <a2.arijitaich@gmail.com>
2021-04-07 04:55:58 +0530

    Update in.py

--- in.py
+++ in.py
@@ -1,18 +1,26 @@
 import moviepy.editor as mp
 
-inp = 'in/XYZ.mp4'
-out = 'out/XYZ.webm'
-
+inp = 'in/f.mp4'
+out = 'out/f.webm'
+aud = 'aud/1.WAV'
 video = mp.VideoFileClip(inp)
 if video.rotation == 90:
     video = video.resize(video.size[::-1])
     video.rotation = 0
-    
+
+
 logo = (mp.ImageClip("logo.png")
           .set_duration(video.duration)
           .resize(height=50) # if you need to resize...
           .margin(right=8, top=8, opacity=0) # (optional) logo-border padding
           .set_pos(("right","top")))
+
+if aud != '':
+    audioclip = mp.AudioFileClip(aud)
+    new_audioclip = mp.CompositeAudioClip([audioclip])
+    video.audio = new_audioclip
+
+
 
 final = mp.CompositeVideoClip([video, logo])
 final.write_videofile(out)
